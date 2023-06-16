fa439b0207f3abf5cee8245bd607131df316517a
UgoPelissier <upelissier@gmail.com>
2023-06-16 11:07:10 +0200

    ➖ Remove comments

--- utils/modelsummary.py
+++ utils/modelsummary.py
@@ -1,21 +1,8 @@
-# Copyright The Lightning AI team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from typing import List, Tuple
 
 from lightning.pytorch.callbacks import ModelSummary
 from lightning.pytorch.callbacks.progress.rich_progress import _RICH_AVAILABLE
-from lightning.pytorch.utilities.model_summary import get_human_readable_count
+from lightning.pytorch.utilities.model_summary.model_summary import get_human_readable_count
 
 if _RICH_AVAILABLE:  # type: ignore[has-type]
     from rich import get_console
@@ -23,40 +10,6 @@ if _RICH_AVAILABLE:  # type: ignore[has-type]
 
 
 class MyRichModelSummary(ModelSummary):
-    r"""
-    Generates a summary of all layers in a :class:`~lightning.pytorch.core.module.LightningModule`
-    with `rich text formatting <https://github.com/Textualize/rich>`_.
-
-    Install it with pip:
-
-    .. code-block:: bash
-
-        pip install rich
-
-    .. code-block:: python
-
-        from lightning.pytorch import Trainer
-        from lightning.pytorch.callbacks import RichModelSummary
-
-        trainer = Trainer(callbacks=RichModelSummary())
-
-    You could also enable ``RichModelSummary`` using the :class:`~lightning.pytorch.callbacks.RichProgressBar`
-
-    .. code-block:: python
-
-        from lightning.pytorch import Trainer
-        from lightning.pytorch.callbacks import RichProgressBar
-
-        trainer = Trainer(callbacks=RichProgressBar())
-
-    Args:
-        max_depth: The maximum depth of layer nesting that the summary will include. A value of 0 turns the
-            layer summary off.
-
-    Raises:
-        ModuleNotFoundError:
-            If required `rich` package is not installed on the device.
-    """
 
     def __init__(self, max_depth: int = 1) -> None:
         if not _RICH_AVAILABLE:
