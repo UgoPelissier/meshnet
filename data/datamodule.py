0537933079b7073d0a9483040190b99eccc54532
Ugo PELISSIER <ugo.pelissier.ext@safrangroup.com>
2023-07-18 15:27:38 +0200

    🩹 Fix path errors

--- data/datamodule.py
+++ data/datamodule.py
@@ -21,7 +21,7 @@ class FreeFemDataModule(pl.LightningDataModule):
         ) -> None:
         super().__init__()
         # Define the indices
-        self.train_idx, self.val_idx, self.test_idx = train_val_test_split(path=path, n=len(os.listdir(osp.join(dataset, "raw", "data"))), val_size=val_size, test_size=test_size)
+        self.train_idx, self.val_idx, self.test_idx = train_val_test_split(path=path, n=len(os.listdir(osp.join(dataset, "raw", "cad"))), val_size=val_size, test_size=test_size)
 
         # Define the dataset
         self.train_dataset = FreeFem(root=dataset, split='train', idx=self.train_idx)
