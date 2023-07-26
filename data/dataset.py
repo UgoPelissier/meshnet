3815ec334f8f6eb0c7a4d2f3942516058c8eb2e4
UgoPelissier <upelissier@gmail.com>
2023-07-26 08:40:35 +0000

    🚧 Adapt dataset processing for geo file

--- data/dataset.py
+++ data/dataset.py
@@ -26,53 +26,20 @@ class FreeFem(Dataset):
 
     @property
     def raw_file_names(self) -> list:
-        return ["stokes_{:03d}".format(i) for i in self.idx]
+        return ["cad_{:03d}".format(i) for i in self.idx]
 
     @property
     def processed_file_names(self) -> list:
-        return glob.glob(os.path.join(self.processed_dir, self.split, 'stokes_*.pt'))
+        return glob.glob(os.path.join(self.processed_dir, self.split, 'cad_*.pt'))
 
     def download(self):
         pass
-
-    def line(
-            self,
-            start: np.ndarray,
-            end: np.ndarray,
-            scale: float
-    ) -> float:
-        """Compute the length of a line."""
-        return scale*float(np.linalg.norm(end-start))
-    
-    def ellipse(
-            self,
-            r1: float,
-            r2: float,
-            scale: float
-    ) -> float:
-        """Compute the length of an ellipse."""
-        return scale*np.sqrt((r1**2 + r2**2)/2)
-
-    def length(
-            self,
-            df: pd.DataFrame
-    ) -> np.ndarray:
-        """Compute the length of the primitive."""
-        length = []
-        for i in range(df.shape[0]):
-            temp = df.iloc[i]
-            if (temp['type'] == 1):
-                length.append(self.line(np.array(temp[['xstart', 'ystart', 'zstart']].values), np.array(temp[['xend', 'yend', 'zend']].values), temp['tend']-temp['tstart']))
-            elif (temp['type'] == 2):
-                length.append(self.ellipse(temp['radius1'], temp['radius2'], temp['tend']-temp['tstart']))
-        return np.array(length)
 
     def process_file(
             self,
             name: str
     ) -> None:
         df = pd.read_csv(osp.join(f'{self.raw_dir}/cad/{name}.txt'), sep='\t')
-        df['length'] = self.length(df)
         df['orientation'] = np.sign(df['n'])
         
         x = torch.tensor(np.array(df.drop(columns=['xstart', 'ystart', 'zstart', 'xend', 'yend', 'zend', 'label', 'n'])), dtype=torch.float32)
