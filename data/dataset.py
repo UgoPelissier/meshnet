2d165f84b1e297c9e61f5bb9125297e43745412d
UgoPelissier <upelissier@gmail.com>
2023-07-26 11:00:58 +0000

    🚧 Adapting dataset processing to gmsh files

--- data/dataset.py
+++ data/dataset.py
@@ -40,22 +40,35 @@ class FreeFem(Dataset):
             name: str
     ) -> None:
         with open(osp.join(self.raw_dir, 'geo', f'{name}.geo'), 'r') as f:
+            # read lines and remove comments
             lines = f.readlines()
             lines = [line.strip() for line in lines]
-            lines = [line for line in lines if line and not line.startswith('//')]
-            lines = [line for line in lines if not line.startswith('Mesh')]
-            lines = [line for line in lines if not line.startswith('Physical')]
-            lines = [line for line in lines if not line.startswith('Field')]
-            lines = [line for line in lines if not line.startswith('Background')]
+            lines = [line for line in lines if not (line.startswith('//') or line.startswith('SetFactory'))]
 
-        df = pd.read_csv(osp.join(f'{self.raw_dir}/cad/{name}.txt'), sep='\t')
-        df['orientation'] = np.sign(df['n'])
-        
-        x = torch.tensor(np.array(df.drop(columns=['xstart', 'ystart', 'zstart', 'xend', 'yend', 'zend', 'label', 'n'])), dtype=torch.float32)
-        pos = torch.tensor(df[['xstart', 'ystart', 'zstart', 'xend', 'yend', 'zend']].values)
-        y = torch.abs(torch.tensor(df['length']/df['n'], dtype=torch.float32))
+            # extract points, lines and circles
+            points = [line for line in lines if line.startswith('Point')]
+            lines__ = [line for line in lines if line.startswith('Line')]
+            circles = [line for line in lines if line.startswith('Circle')]
 
-        torch.save(Data(x=x, pos=pos, y=y, name=torch.tensor(int(name[-3:]), dtype=torch.long)), osp.join(self.processed_dir, self.split, f'{name}.pt'))
+            # extract coordinates and mesh size
+            points = torch.Tensor([[float(p) for p in line.split('{')[1].split('}')[0].split(',')] for line in points])
+            y = points[:, -1]
+            pos = points[:, :-1]
+
+            # extract edges
+            lines__ = torch.Tensor([[int(p) for p in line.split('{')[1].split('}')[0].split(',')] for line in lines__]).long()
+            circles = torch.Tensor([[int(p) for p in line.split('{')[1].split('}')[0].split(',')] for line in circles]).long()[:,[0,2]]
+            edges = torch.cat([lines__, circles], dim=0)
+            receivers = torch.min(edges, dim=1).values
+            senders = torch.max(edges, dim=1).values
+            packed_edges = torch.stack([senders, receivers], dim=1)
+            # remove duplicates and unpack
+            unique_edges = torch.unique(packed_edges, dim=0)
+            senders, receivers = unique_edges[:, 0], unique_edges[:, 1]
+            # create two-way connectivity
+            edge_index = torch.stack([torch.cat((senders, receivers), dim=0), torch.cat((receivers, senders), dim=0)], dim=0)
+
+            torch.save(Data(x=x, edge_index=edge_index, pos=pos, y=y, name=torch.tensor(int(name[-3:]), dtype=torch.long)), osp.join(self.processed_dir, self.split, f'{name}.pt'))
 
     def process(self) -> None:
         """Process the dataset."""
