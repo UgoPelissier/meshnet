fd5e61ad4c60e2deefcba7a3467c5a35b27db30c
UgoPelissier <upelissier@gmail.com>
2023-09-25 16:48:50 +0000

    🐛 Correct number of obstacles calculation

--- data/dataset.py
+++ data/dataset.py
@@ -162,7 +162,7 @@ class CAD(Dataset):
             # Extract edges
             edges = {}
             tmp = [line for line in lines if line.startswith("Line(") or line.startswith("Spline(")]
-            n_cyl = int(len([line for line in lines if line.startswith("Spline(")])/(1+self.dim))
+            n_cyl = int(len([line for line in lines if line.startswith("Spline(")])/(self.dim-1))
             for line in tmp:
                 key = line.split('(')[1].split(')')[0]
                 value = line.split('{')[1].split('}')[0].split(',')
